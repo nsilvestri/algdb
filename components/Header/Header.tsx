da7bccdd445f586b89cdabe1066dd0f68ba3f3ad
Nick Silvestri <nickpsilvestri@gmail.com>
2023-08-31 00:04:31 -0400

    Adjust header color

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -7,7 +7,7 @@ import prisma from "@/prisma/global-prisma-client";
 export async function Header() {
   const puzzles = await prisma.puzzle.findMany();
   return (
-    <header className="flex justify-between content-center p-3 bg-slate-300 dark:bg-slate-900">
+    <header className="flex justify-between content-center p-3 bg-slate-200 dark:bg-slate-900">
       <div className="flex gap-2">
         <Link href="/">
           <h1 className="font-bold text-4xl">AlgDB</h1>
