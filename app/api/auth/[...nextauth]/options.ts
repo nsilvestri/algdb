ee4277d437b01aaef85c2f4cb9e5103fd4417020
Eric Xu <ericxu2048@gmail.com>
2023-09-15 23:27:02 -0400

    Add WCA OAuth

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -6,6 +6,31 @@ import prisma from "@/prisma/global-prisma-client";
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
+    {
+      // WCA OAuth Provider
+      // Docs: https://docs.worldcubeassociation.org/knowledge_base/v0_api.html
+      id: "wca",
+      name: "WCA",
+      type: "oauth",
+      authorization: {
+        url: "https://www.worldcubeassociation.org/oauth/authorize",
+        params: {
+          scope: "public",
+        },
+      },
+      token: "https://www.worldcubeassociation.org/oauth/token",
+      userinfo: "https://www.worldcubeassociation.org/api/v0/me",
+      profile(profile) {
+        return {
+          id: profile.me.id,
+          name: profile.me.name,
+          image: profile.me.avatar.thumb_url,
+          // TODO: get WCA ID
+        };
+      },
+      clientId: process.env.WCA_CLIENT_ID!,
+      clientSecret: process.env.WCA_CLIENT_SECRET!,
+    },
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
