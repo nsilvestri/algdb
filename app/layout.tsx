9e5456bca9bb2ac41f7811d39ba2b3ced5c2c7cf
Eric Xu <ericxu2048@gmail.com>
2023-09-23 23:54:32 -0400

    Fix CSS issues causing homepage overflow

--- app/layout.tsx
+++ app/layout.tsx
@@ -81,7 +81,7 @@ export default async function RootLayout({
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <ClientSessionProvider session={session}>
             <div className="flex min-h-screen flex-col">
-              <header className="sticky top-0 z-40 w-full border-b bg-background">
+              <header className="sticky top-0 z-40 w-full h-16 box-border border-b bg-background">
                 <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
                   <MainNav items={mainNavItems}>
                     <SidebarNav items={sidebarNavItems} />
@@ -103,10 +103,10 @@ export default async function RootLayout({
               </header>
               <div className="container flex-1">
                 <div className="flex-1 md:grid md:grid-cols-[220px_1fr] md:gap-6 lg:grid-cols-[240px_1fr] lg:gap-10">
-                  <aside className="fixed top-14 z-30 hidden h-[calc(100vh-3.5rem)] w-full shrink-0 overflow-y-auto border-r py-6 pr-2 md:sticky md:block lg:py-10">
+                  <aside className="fixed top-14 z-30 hidden h-[calc(100vh-4rem)] w-full shrink-0 overflow-y-auto border-r py-6 pr-2 md:sticky md:block lg:py-10">
                     <SidebarNav items={sidebarNavItems} />
                   </aside>
-                  <div className="mt-4 h-full">{children}</div>
+                  <div className="pt-4 h-full">{children}</div>
                 </div>
               </div>
             </div>
