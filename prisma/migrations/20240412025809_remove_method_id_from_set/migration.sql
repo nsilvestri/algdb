c815cf274b5b344281fefeeec8ce0709485d6346
Nick Silvestri <nickpsilvestri@gmail.com>
2024-04-11 23:18:55 -0400

    Remove methodId from set schema
    This value in the schema was mistakenly not removed with other
    changes to the schema that happened.

---
+++ prisma/migrations/20240412025809_remove_method_id_from_set/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `methodId` on the `Set` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Set" DROP COLUMN "methodId";
